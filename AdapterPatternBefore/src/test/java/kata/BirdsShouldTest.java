b23855c0c562d4e3eb3d77ec45b728e4438610b7
Barrus, Jeffrey <jeffrey.barrus@fmr.com>
2024-01-09 11:14:45 -0700

    remove implementation, update readme

--- AdapterPatternBefore/src/test/java/kata/BirdsShouldTest.java
+++ AdapterPatternBefore/src/test/java/kata/BirdsShouldTest.java
@@ -8,15 +8,12 @@ public class BirdsShouldTest {
 
     @Test
     public void make_ALL_Birds_create_a_Melody_BY_list() {
-        Bird duck = new DuckBirdAdapter(new Duck());
-        Bird potato = new MagicPotatoBirdAdapter(new MagicPotato());
-        Bird bat = new BatBirdAdapter(new Bat());
         Bird sparrow = new Sparrow();
         Bird canary = new Canary();
 
         Melody melody = new Melody();
-        // TODO add bat, duck and magic potato
-        melody.createSong(new Bird[]{bat, canary, duck, potato, sparrow});
+        // TODO add bat, canary, duck, potato, sparrow
+        melody.createSong(new Bird[]{canary, sparrow});
 
         Melody melodyExpected = new Melody("I am the Night Melodious PIO PIO Cuack Cuack Magic potato RULES Chirp Chirp");
 
@@ -27,20 +24,16 @@ public class BirdsShouldTest {
 
     @Test
     public void make_ALL_birds_teach_Flylessons_BY_list() {
-        Bird duck = new DuckBirdAdapter(new Duck());
-        Bird potato = new MagicPotatoBirdAdapter(new MagicPotato());
-        Bird bat = new BatBirdAdapter(new Bat());
         Bird sparrow = new Sparrow();
         Bird canary = new Canary();
 
         FlyTime flylesson = new FlyTime();
         // TODO list should have bat, canary, potato, sparrow, duck
-        flylesson.createFlylesson(new Bird[]{bat, canary, duck, potato, sparrow});
+        flylesson.createFlylesson(new Bird[]{canary, sparrow});
 
         FlyTime flyLessonExpected = new FlyTime("Fliying in the Darkness Flying like a canary Flying more or less rolling potato Flying to steal your Bread");
 
         flylesson.printFlyLessons();
-
 
         assertEquals(flyLessonExpected.flyLesson, flylesson.flyLesson);
     }
--- README.md
+++ README.md
@@ -10,65 +10,15 @@ You need to use the Adapter Pattern to find a solution.
 
 ## Getting Started
 
-You have two different projects
-
-AdapterPatternBefore Base code of the KATA
-
-AdapterPatternAfter  Solution of the Kata
+Check out `start` branch and run maven package on the `[AdapterPatternBefore](AdapterPatternBefore)` folder.
 
 ### Prerequisites
 
-Eager to learn!!
+maven, java
 
 ## Running the tests
 
-Test that show how you can create a song
-
-```[java]
-
-@Test
-public void make_ALL_Birds_create_a_Melody() {
-
-    kata.Bird isCaptain = new kata.Sparrow();
-    kata.Bird canary = new kata.Canary();
-
-    String song = canary.sing()+" "+isCaptain.sing();
-
-    kata.Melody melody = new kata.Melody(song);
-    kata.Melody melodyExpected = new kata.Melody("Melodious PIO PIO Chirp Chirp");
-
-    melody.printMelody();
-
-    assertEquals(melodyExpected.melody,melody.melody);
-}
-
-```
-
-Test that show how you can create a Fly Lesson
-
-```[java]
-@Test
-public void make_ALL_birds_teach_Flylessons(){
-
-    kata.Bird isCaptain = new kata.Sparrow();
-    kata.Bird canary = new kata.Canary();
-
-    String lesson =canary.fly()+" "+isCaptain.fly();
-    kata.FlyTime flylesson = new kata.FlyTime(lesson);
-    kata.FlyTime flyLessonExpected = new kata.FlyTime("Flying like a canary Flying to steal your Bread");
-
-    flylesson.printFlyLessons();
-
-    assertEquals(flyLessonExpected.flyLesson,flylesson.flyLesson);
-}
-
-```
-
-
-## Built With
-
-* [Maven](https://maven.apache.org/) - Dependency Management
-
+`mvn test` or run in IDE
 
 ## Authors
 
